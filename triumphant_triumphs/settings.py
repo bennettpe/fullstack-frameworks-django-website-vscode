8728516f60c5b237e0ff8a9b3d5da99b27130ea9
bennettpe <37184112+bennettpe@users.noreply.github.com>
2020-02-23 08:43:51 +0000

    Add new host to allowed_hosts

--- triumphant_triumphs/settings.py
+++ triumphant_triumphs/settings.py
@@ -38,7 +38,7 @@ else:
 
 
 # A list of strings representing the host/domain names that this Django site can serve
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'fullstack-frameworks-django-vscode-project.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'fullstack-frameworks-vscode.herokuapp.com']
 
 # To load static files to S3
 #ALLOWED_HOSTS = ['27641ebaf5074751b3a2e3edba1811b2.vfs.cloud9.us-east-1.amazonaws.com']
